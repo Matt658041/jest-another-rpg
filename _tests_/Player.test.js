ca6d02e2846c87d55ffdc37956e9628570339cf0
Matt658041 <91630948+Matt658041@users.noreply.github.com>
2022-04-11 10:35:22 -0500

    object passed test

--- _tests_/Player.test.js
+++ _tests_/Player.test.js
@@ -1,6 +1,12 @@
+const Potion = require('../lib/Potion');
+
+jest.mock('../lib/Potion.js');
+
+console.log(new Potion());
+
 const Player = require('../lib/Player');
 
-jest.mock('.../lib/Potion');
+
 
 test ('create a player object', () => {
     const player = new Player ('Dave');
@@ -9,5 +15,8 @@ test ('create a player object', () => {
     expect(player.health).toEqual(expect.any(Number));
     expect (player.strength).toEqual (expect.any(Number));
     expect (player.agility).toEqual (expect.any(Number));
-
+    expect(player.inventory).toEqual(
+        expect.arrayContaining([expect.any(Object)])
+      );
+   
 });
